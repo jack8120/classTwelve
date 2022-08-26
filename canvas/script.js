280d970962fd5d8bd952676290f380b992882036
Jack Torn <j4cktorn@gmail.com>
2022-08-26 19:55:01 +0100

    addded line and circle to canvas

--- canvas/script.js
+++ canvas/script.js
@@ -2,8 +2,21 @@
 
 let canvas = document.getElementById("myCanvas");
 
-let ctx = canvas.getContext("2d");//
+let ctx = canvas.getContext("2d");
 
+
+//Rectangle//
 ctx.fillStyle = "red";
 
-ctx.fillRect(0,0,120,50);
+ctx.fillRect(0,0,150,75);
+
+// Line//
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
+
+//Circle//
+
+ctx.beginPath();
+ctx.arc(200,100,100,0,2 * Math.PI);
+ctx.stroke();
