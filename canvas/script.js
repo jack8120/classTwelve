d6633d181b89aa64d3c693762ad0795e45ea4b01
Jack Torn <j4cktorn@gmail.com>
2022-08-26 20:27:11 +0100

    gradient

--- canvas/script.js
+++ canvas/script.js
@@ -5,18 +5,32 @@ let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
 
-//Rectangle//
+//Rectangle
 ctx.fillStyle = "red";
 
 ctx.fillRect(0,0,150,75);
 
-// Line//
+// Line
 ctx.moveTo(0,0);
 ctx.lineTo(200,100);
 ctx.stroke();
 
-//Circle//
+//Circle
 
 ctx.beginPath();
 ctx.arc(200,100,100,0,2 * Math.PI);
 ctx.stroke();
+
+//Gradient
+
+let grd = ctx.createLinearGradient(0,0,800,90);
+grd.addColorStop(0, "red");
+grd.addColorStop(0.25, "blue");
+grd.addColorStop(0.5, "green");
+grd.addColorStop(0.75, "yellow");
+grd.addColorStop(0.8, "white");
+
+// Fill with gradient
+
+ctx.fillStyle = grd;
+ctx.fillRect(0,200,600,400);
